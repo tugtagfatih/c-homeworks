f848bf54575b1979c94c6ccf5210034b207be708
tugtagfatih <117814951+tugtagfatih@users.noreply.github.com>
2024-03-14 11:32:44 +0300

    Uppercase movement restored and some comments added

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -54,7 +54,7 @@ void print_board(){
 void move_player(){
     printf("Enter your move: ");
     scanf("%c", &movement);
-    if (movement == 'd' && player_point % 16 != 15){
+    if ((movement =='D' || movement == 'd') && player_point % 16 != 15){
         if( (ects >=24 || (player_point!= 121 && player_point!= 137 && player_point!= 153 )) && (ects >=55 || (player_point!= 91 && player_point!= 107 && player_point!= 123 &&player_point!= 139 && player_point!= 155 && player_point!= 171 && player_point!= 187))){
             if (lab[player_point+1] == '1' || lab[player_point+1] == '2') ects = ects +8;
             lab[player_point] = '.';
@@ -63,7 +63,7 @@ void move_player(){
         }
         else printf("Insufficient ects, please try again. \n");
     }
-    else if (movement =='a' && player_point % 16 !=0 ){
+    else if ((movement == 'A' || movement =='a') && player_point % 16 !=0 ){
         if( ( ects>=24 || (player_point != 119 && player_point != 135 && player_point != 151)) && (ects >=55 || (player_point != 85 && player_point != 101 && player_point != 117 && player_point != 133 && player_point != 149 && player_point != 165 && player_point != 181 ))){
             if (lab[player_point-1] == '1' || lab[player_point-1] == '2') ects = ects +8;
             lab[player_point] = '.';
@@ -72,7 +72,7 @@ void move_player(){
         }
         else printf("Insufficient ects, please try again. \n");
     }
-    else if (movement == 'w' && player_point > 15){
+    else if ((movement == 'W' || movement == 'w') && player_point > 15){
         if ( (ects>=24 || (player_point!= 119 && player_point!= 120 && player_point!= 121 )) && (ects >= 55 || (player_point != 85 && player_point != 86 && player_point != 87 && player_point != 88 && player_point != 89 && player_point != 90 && player_point != 91 ))){
             if (lab[player_point-16] == '1' || lab[player_point-16] == '2') ects = ects +8;
             lab[player_point] = '.';
@@ -81,7 +81,7 @@ void move_player(){
         }
         else printf("Insufficient ects, please try again. \n");
     }
-    else if (movement == 's' && player_point < 240){
+    else if ((movement == 'S' || movement == 's') && player_point < 240){
         if ( (ects>=24 || (player_point!= 151 && player_point!= 152 && player_point!= 153 )) && (ects >= 55 || (player_point != 181 && player_point != 182 && player_point != 183 && player_point != 184 && player_point != 185 && player_point != 186 && player_point != 187 ))){
             if (lab[player_point +16] == '1' || lab[player_point+16] == '2') ects = ects +8;
             lab[player_point] = '.';
@@ -106,16 +106,10 @@ void initalize_game(){
         for (looph; looph <=15; looph++){
             i++;
             j++;
-            if ((loopv>=4 && loopv<=12 && i >=4 && j==4) || (loopv>=4 && loopv<=12 && i >=4 && j==12) || ((loopv==4 || loopv==12) && (i ==4 || (j<=12 && j >=4)))){ /*outer walls*/
-                lab[i] = '#';
-            }
-            if ((loopv>=6 && loopv<=10 && i >=6 && j==6) || (loopv>=6 && loopv<=10 && i >=6 && j==10) || ((loopv==6 || loopv==10) && (i ==6 || (j<=10 && j >=6)))){ /*inner walls*/
-                lab[i] = '#';
-            }
             if ((loopv>=5 && loopv<=11 && i >=5 && j==5) || (loopv>=5 && loopv<=11 && i >=5 && j==11) || ((loopv==5 || loopv==11) && (i ==5 || (j<=11 && j >=5)))){
                 int two_creator = rand() %6; /*There are 24 different squares where the numbers 2 can appear, and since there should be 4, I give the probability 1/6.*/
                 two_max--;
-                if (two_counter<=2 && (two_creator == 5 || two_max <=3)){
+                if (two_counter<=2 && (two_creator == 5 || two_max <=3)){ /*checks all last 3 characters and if there are fewer '2' characters than the number of remaining spaces, it fills the remaining spaces with '2'*/
                     lab[i] = '2';
                     two_counter++;
                 }
@@ -123,7 +117,7 @@ void initalize_game(){
             if ((loopv>=7 && loopv<=9 && i >=7 && j==7) || (loopv>=7 && loopv<=9 && i >=7 && j==9) || ((loopv==7 || loopv==9) && (i ==7|| (j<=9 && j >=7)))){
                 int one_creator = rand() %2;
                 one_max--;
-                if (one_counter <4 && (one_creator ==1 || one_max <=3)){
+                if (one_counter <4 && (one_creator ==1 || one_max <=3)){ /*checks all last 4 characters and if there are fewer '1' characters than the number of remaining spaces, it fills the remaining spaces with '1'*/
                     lab[i] = '1';
                     one_counter++;
                 }

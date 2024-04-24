35e2e92e69481962380ca60963980dfed42c815d
tugtagfatih <117814951+tugtagfatih@users.noreply.github.com>
2024-04-24 16:45:49 +0300

    last version of hw7.c

--- hw7/hw7.c
+++ hw7/hw7.c
@@ -2,25 +2,52 @@
 
 void initializeGame(int board[2][7]);
 void printBoard(int board[2][7]);
-void gamePlay(int board[2][7]);
-int move(int board[2][7],int cup,int turn);
+void gamePlay(int board[2][7], int turn);
+void move(int board[2][7],int cup,int turn);
+int check_end(int board[2][7]);
+
+int check_end(int board[2][7]){
+    int ret;
+    if ((board[0][0]==0 && board[0][1]==0 && board[0][2]==0 &&  board[0][3]==0 &&  board[0][4]==0 &&  board[0][5]==0 ) || (board[1][0]==0 && board[1][1]==0 && board[1][2]==0 &&  board[1][3]==0 &&  board[1][4]==0 &&  board[1][5]==0))
+    {ret=1;}
+    else ret=0;
+    if (ret==1){
+        if (board[0][6]>board[1][6]){
+            printf("+-------------------------------------------------------------+\n");
+            printf("+------------------- Player wins the game!!! -----------------+\n");
+        }
+        else if (board[0][6]<board[1][6]){
+            printf("+-------------------------------------------------------------+\n");
+            printf("+------------------- Computer wins the game!!! ---------------+\n");
+        }
+        else{
+            printf("+-------------------------------------------------------------+\n");
+            printf("+------------------- The game is a draw!!! -------------------+\n");
+        }
+    }
+    return ret;
+}
+
+int main(){
+    int board[2][7]; /* 0-5: player 1, 6: store 1, 7-12: player 2, 13: store 2*/
+    initializeGame(board);
+    printBoard(board);
+    gamePlay(board, 1); /*1 for player. 2 for computer.*/
+    return 0;
+}
 
 void initializeGame(int board[2][7]){
     int i,n=0;
     while (n<1){
         printf("Please enter the number of stones for each cup: ");
         scanf("%d", &n);
-        if(n>1) {
+        if(n>=1) {
             for(i = 0; i < 7; i++){
-                if (i == 6){
-                    board[0][i] = 0;
-                    board[1][i] = 0;
-                }
-                else {
-                    board[0][i] = n;
-                    board[1][i] = n;
-                }
+                board[0][i] = n;
+                board[1][i] = n;
             }
+            board[0][6] = 0;
+            board[1][6] = 0;
         }
         else printf ("Game size can't be less than 1\n");
     }
@@ -38,141 +65,74 @@ void printBoard(int board[2][7]){
     printf("Cups:           1     2     3     4     5     6\n");
 }
 
-void gamePlay(int board[2][7]){
-    int first_turn = 1, turn=1, cup,i,largestnum=0,largestadress;
-    while (turn!=0){
-        largestadress=0;
-        largestnum=0;
-        cup=0;
-        if (turn == 1){
-            printf("Player's turn\n");
-            while (cup < 1 || cup > 6 || board[0][cup-1]==0){
-                printf("Please enter the cup number you want to move: ");
-                scanf("%d", &cup);
-                getchar();
-                if (cup < 1 || cup > 6 || board[0][cup-1]==0){
-                    printf("Invalid cup number\n");
-                }
+void gamePlay(int board[2][7] ,int turn){
+    int cup=0,i,largestnum=0,largestadress,j;
+    if (turn==1){
+        printf("Player's turn\n");
+        while (cup < 1 || cup > 6 || board[0][cup-1]==0){
+            printf("Please enter the cup number you want to move: ");
+            scanf("%d", &cup);
+            getchar();
+            if (cup < 1 || cup > 6 || board[0][cup-1]==0){
+                printf("Invalid cup number\n");
             }
-            turn=move(board, cup, turn);
         }
-        if (turn == 2){
-            printf("Computer's turn\n");
-            for(i=7 ; i<13;i++){
-                cup=i;
-                if (board[1][i-7]!=0 && 6-board[1][i-7]==0 ){
-                    printf("Computer's move: %d\n",i-6);
-                    move(board, cup, turn);
-                    break;
-                }
-                if (board[1][i-7]>largestnum){
-                    largestnum=board[1][i-7];
-                    largestadress=i;
-                }
+        move(board, cup-1, turn);
+    }
+
+    else if (turn==2){
+        printf("Computer's turn\n");
+        for(i=0 ; i<6 ; i++){
+            if (board[1][i]!=0){
+                if (6-board[1][i]==0) largestnum=0,largestadress=i;
+                if(largestnum < board[1][i])largestnum = board[1][i],largestadress=i;
             }
-            if (largestadress==0) largestadress=7;
-            while (1) {
-                if (board[1][largestadress-7]==0){
-                    largestadress++;
-                    if (largestadress==13) largestadress=7;
-                }
-                else break;
-            }
-            printf("Computer's move: %d\n",largestadress-6);
-            turn=move(board, largestadress, turn);
         }
-        if((board[0][0]==0 && board[0][1]==0 && board[0][2]==0 && board[0][3]==0 && board[0][4]==0 && board[0][5]==0) || (board[1][0]==0 && board[1][1]==0 && board[1][2]==0 && board[1][3]==0 && board[1][4]==0 && board[1][5]==0)){
-            turn=0;
-            if (board[0][6]>board[1][6]){
-                printf("+-------------------------------------------------------------+\n");
-                printf("+------------------- Player wins the game!!! ----------------+\n");
-            }
-            else if (board[0][6]<board[1][6]){
-                printf("+-------------------------------------------------------------+\n");
-                printf("+------------------- Computer wins the game!!! --------------+\n");
-            }
-            else{
-                printf("+-------------------------------------------------------------+\n");
-                printf("+------------------- The game is a draw!!! ------------------+\n");
-            }
+        if (largestadress<13){
+            move(board,largestadress+7,2);
+        }
+        else {
+            for(i=0; i<6; i++) if (board[1][i]!=0) largestadress=i;
+            move(board,largestadress+7,2);
         }
     }
-    
 }
 
-int move(int board[2][7],int cup,int turn){
-    int i,j, n, result, bos;
-    int otherplayer=0;
-    i = cup;
-    if (turn == 1){ /*TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1 TURN1*/
-        result=2;
-        n = board[0][cup-1];
-        board[0][cup-1] = 0;
-        for (j=0; j < n; j++){
-            if (i<7){
-                board[0][i] += 1;
-            }
-            else if (i>=7 && i<13){
-                board[1][i-7] += 1;
-            }
-            else j--; /*when i==13*/
-            i++;
-            if (i == 14) i=0;
-        }
-        if ((board[0][i] >1 || board[1][i-7] >1)&& i!=7){
-            
-            printBoard(board);
-            printf("Next move is: %d\n",i);
-            move(board, i, turn);
-        }
-        else if (i==7){
-            printBoard(board);
-            while (1){
-                printf("Your turn finished on your big cup. Please Select a cup: ");
-                scanf("%d", &cup);
-                if (cup>0 && cup<7){
-                    if (board[0][cup-1]>=1){
-                        move(board, cup, turn);
-                    }
-                    else printf("Invalid cup number\n");
-                }
-                else printf("Invalid cup number\n");
-            }
-        }
-        else printBoard(board);
-    }
-    if (turn == 2){        
-        /*printf("Computer's move: %d\n",cup);*/
-        result=1;
-        n = board[1][cup-7];
-        board[1][cup-7]=0;
-        i=cup+1;
-        for (n; n > 0; n--){
-            if (i<6){
-                board[0][i] += 1;
-            }
-            else if (i>6 && i<=13){
-                board[1][i-7] += 1;
-            }
-            else j--;
-            i++;
-            if (i == 14) i=0;
-        }
-        if (i==0){
-            result=2;
-        }
-        else if (board[1][i-7] >1 || board[0][i] >1){
-            printBoard(board);
-            move(board, i, turn);
+void move(int board[2][7],int cup,int turn){
+    int i,j,n;
+    if(turn==1){
+        j=cup+1;
+        n=board[0][cup];
+        board[0][cup]=0;
+        for (i=0; i<n ; i++){
+            if (j<7) board[0][j]+=1;
+            else if(j>=7 && j<13) board[1][j-7] +=1;
+            else i--;
+            j++;
+            if(j==14) j=0;
         }
         printBoard(board);
+        j=j-1;
+        if (check_end(board)==1) {gamePlay(board, 0);}
+        else if(j==6) gamePlay(board,1);
+        else if((j<7 && board[0][j] > 1) || ((j>=7 && j<13) && board[1][j-7] >1)) move(board, j,1);
+        else gamePlay(board,2);
     }
-    return result;
-}
-int main(){
-    int board[2][7]; /* 0-5: player 1, 6: store 1, 7-12: player 2, 13: store 2*/
-    initializeGame(board);
-    printBoard(board);
-    gamePlay(board);
-    return 0;
+    if (turn==2){
+        j=cup+1;
+        n=board[0][cup];
+        board[0][cup]=0;
+        for (i=0; i<n ; i++){
+            if (j<6) board[0][j]+=1;
+            else if(j>=7 && j<=13) board[1][j-7] +=1;
+            else i--;
+            j++;
+            if(j==14) j=0;
+        }
+        printBoard(board);
+        if (check_end(board)==1) {gamePlay(board, 0);}
+        else if(j==0) gamePlay(board,2);
+        else if((j<7 && board[0][j-1] > 1) || ((j>=7 && j<13) && board[1][j-8] >1)) move(board, j-1,2);
+        else gamePlay(board, 1);
+    }
 }
